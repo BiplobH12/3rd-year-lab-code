3ca35ac1f3aba7607d510cfcd1c026bc060cb448
Biplob hossen <154337092+BiplobH12@users.noreply.github.com>
2025-12-06 00:48:52 +0600

    Sum of log(1+x) series

---
+++ log (1+x) seris .cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double l(double x, int n){
+    double sum = 0;
+    int i = 1;           
+    while(n--){
+        double t =pow(x, i)*pow(-1,i+1) / i;
+        sum += t;
+        i++; 
+    }
+    return sum;
+}
+int main(){
+z:
+    double x;
+    int n, y;
+    cout <<fixed<<setprecision(20);
+    cout << "Enter the value of x and n" << endl;
+    cin >> x >> n;
+    cout << "Sum of log(1+x) series = " << l(x, n) << endl;
+    cout <<"Error  of series value  " <<log(1+x)-l(x,n)<< endl;
+    cout << "Run Again? yes 1 or no 0" << endl;
+    cin >> y;
+    if(y) goto z;
+}
