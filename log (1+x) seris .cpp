131a646e31a740f9b6e00e35128a48545b310e1e
Biplob hossen <154337092+BiplobH12@users.noreply.github.com>
2025-12-07 23:21:44 +0600

    Adjust log(1+x) series output for natural log conversion

--- log (1+x) seris .cpp
+++ log (1+x) seris .cpp
@@ -18,7 +18,7 @@ z:
     cout <<fixed<<setprecision(20);
     cout << "Enter the value of x and n" << endl;
     cin >> x >> n;
-    cout << "Sum of log(1+x) series = " << l(x, n) << endl;
+    cout << "Sum of log(1+x) series = " << l(x, n)/2.302585 << endl;
     cout <<"Error  of series value  " <<log(1+x)-l(x,n)<< endl;
     cout << "Run Again? yes 1 or no 0" << endl;
     cin >> y;
