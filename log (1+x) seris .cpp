fc5868d6e06ad13b93b2f0fec9938fee4ebe93e1
Biplob hossen <154337092+BiplobH12@users.noreply.github.com>
2025-12-08 08:57:44 +0600

    Calculated log(1+x) series

--- log (1+x) seris .cpp
+++ log (1+x) seris .cpp
@@ -18,8 +18,13 @@ z:
     cout <<fixed<<setprecision(20);
     cout << "Enter the value of x and n" << endl;
     cin >> x >> n;
+    if(x<0){
+      cout<<"Series doesn’t converge "<<endl;
+    }
+    else {
     cout << "Sum of log(1+x) series = " << l(x, n)/2.302585 << endl;// log(1+x)=In(1+x)/2.302585
     cout <<"Error  of series value  " <<log10(1+x)-(l(x,n)/2.302585)<< endl;
+    }
     cout << "Run Again? yes 1 or no 0" << endl;
     cin >> y;
     if(y) goto z;
